44e3d6aac22ee87df17530f157d79ff2ad2a7ad5
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2011-12-19 14:25:23 +0100

    Participate page

---
+++ templates_compiled/js/participate.js
@@ -0,0 +1,3 @@
+Joshfire.define([],function() {return function(obj,_) {
+var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1 id="titre">Participate!</h1>\n\n<p>Blaa</p>');}return __p.join('');
+}});
